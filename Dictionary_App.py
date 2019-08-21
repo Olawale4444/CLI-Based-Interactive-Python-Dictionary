6a4f74b72e53e7c9139f68fbe2b1a2d24ee5bb62
Olawale4444 <31905212+Olawale4444@users.noreply.github.com>
2019-08-21 15:05:17 +0100

    Add files via upload

---
+++ Dictionary_App.py
@@ -0,0 +1,52 @@
+
+
+#Python Dictionary Program
+#Olowonyo Olawale
+#twitter @wale_io 
+
+import json
+from difflib import get_close_matches
+#difflib is a library which helps find matching or closely matching words
+
+sourceData = json.load(open("data.json"))
+#This opens the data.json file and stores it to the variable sourceData 
+
+
+def query(w):
+    w = w.lower()
+    if w in sourceData:
+        print("Heres the Definition of {} below \n".format(w))
+        print(sourceData[w]) 
+        # return sourceData[w]
+        print("\n")
+    #if the word, which is the "key" is in the source data file, it returns the "value" pair
+
+    elif len(get_close_matches(w, sourceData.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, sourceData.keys())[0])
+        if yn == "Y":
+            return sourceData[get_close_matches(w, sourceData.keys())[0]]
+            #Checks for close matches with the library 
+
+
+        elif yn == "N":
+            return "This word does not exist in the Dictionary. Please try again."
+        else:
+            return "Wrong Input."
+    else:
+        return "The word doesn't exist. Please double check it."
+        
+
+ 
+rt = True
+#condition for while loop, for the dictionary to keep asking the user for input.
+
+while rt ==True:
+    print('\n The Best CLI Dictionary on the Planet,------Press "N" to quit this Dictionary \n')
+
+    word = input("Input your word below here my Good friend :\n ")
+    if word == "N":
+
+        rt == False 
+        break
+    else:
+        query(word) 
